3b247d6bd3c50860b99ab268cffa9cbfc8d86cea
Rohith kumar <rohith@victopialabs.com>
2024-06-09 15:14:47 +0530

    completed complaint and calendar page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Secretary from './pages/Secretary';
-import Calendar from './pages/Calendar';
-import Complaint from './pages/Complaint';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Secretary from './pages/Secretary.jsx';
+import Calendar from './pages/Calendar.jsx';
+import Complaint from './pages/Complaint.jsx';
+import Home from './pages/Home.jsx';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/complaint' element={<Complaint />} />
         <Route path='/calendar' element={<Calendar />} />
         <Route path='/secretary' element={<Secretary />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Secretary.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Secretary = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Secretary
